eced2c9bfe04df21fc7e0f01aff3ef1f870f82df
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-29 23:53:05 +0600

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+  
+
+  return (
+    <>
+      <header>
+       <DaisyNav></DaisyNav>
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
